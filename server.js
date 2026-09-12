ce7dbf05a561269755d1e7ee93ac2a39d2b392c2
Rohit kumar <rohitkumar9643017@gmail.com>
2026-09-13 00:49:07 +0530

    update cors error

--- server.js
+++ server.js
@@ -1,24 +1,27 @@
-import express from "express"
-import dotenv from "dotenv"
-import connectDB from "./config/db.js"
-import router from "./routes/mainRoute.js"
-import cors from 'cors'
+import express from "express";
+import dotenv from "dotenv";
+import connectDB from "./config/db.js";
+import router from "./routes/mainRoute.js";
+import cors from "cors";
 
-dotenv.config()
+dotenv.config();
 
-connectDB()
-const app=express()
+connectDB();
+const app = express();
 // --- Allowed Frontends ---
 const allowedOrigins = [
-  "http://localhost:3000",        // local dev
-  "http://localhost:3001",        // local dev
-  "https://testpro-exams.vercel.app"    // live frontend
+  "https://testpro-exams.vercel.app", // live frontend
+  "http://localhost:3000", // local dev
+  "http://localhost:3001", // local dev
 ];
 
-app.use(cors());
-app.use(express.json())
-app.use("/api", router)
-
+app.use(
+  cors({
+    origin: allowedOrigins,
+  }),
+);
+app.use(express.json());
+app.use("/api", router);
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
